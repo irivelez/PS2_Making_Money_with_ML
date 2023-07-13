dbc1834a949766c65d40b63c0878a8907fa2652e
MiguelVictoria <m.victorias@uniandes.edu.co>
2023-07-13 12:00:40 -0500

    Update 1. Loading data.R

--- scripts/1. Loading data.R
+++ scripts/1. Loading data.R
@@ -84,15 +84,24 @@ class(combine)
 
 # View data ---------------------------------------------------------------
 
-## Variable de interes (precio)
+## Datos en general
+glimpse(train)
+
+
+## Grafica - Variable de interes (precio)
+# Su distribución es más como una bernulli
 ggplot(train, aes(x = price)) +
   geom_histogram(fill = "darkblue") +
   theme_bw() +
   labs(x = "Precio de arriendo", y = "Cantidad")
 
+## ## Descriptivas - Variable de interes (precio)
+summary(train$price) %>%
+  as.matrix() %>%
+  as.data.frame() %>%
+  mutate(V1 = scales::dollar(V1))
 
 # Ubicacion Chapinero -----------------------------------------------------
-
 
 #Creamos el mapa interactivo con la biblioteca leaflet agregando círculos 
 #a partir de las coordenadas de datos de nuestra base "combine"
@@ -129,8 +138,6 @@ leaflet() %>% addTiles() %>% addCircles(data=combine_chapinero) %>% addPolygons(
 available_features()
 available_tags("amenity")
 
-
-
 # Description data --------------------------------------------------------
 
 ### Aquí la idea es crear algunas variables que identifiquemos de la base de datos y nos puedan
@@ -138,7 +145,7 @@ available_tags("amenity")
 ### convertir ese texto en variable. El problem set nos pide dos variables de las bases de datos
 ### y cuatro variables que tendremos que sacar de otro lado
 
-### Variable número 1: Parqueadero
+### Variable número 1: Parqueadero ####
 Descripc <- combine_chapinero$description
 parqueaderoT_aux1<-str_detect( Descripc,"parqueadero")
 parqueaderoT_aux2<-str_detect( Descripc,"parqueaderos") 
@@ -150,17 +157,26 @@ parqueaderoT_aux7<-str_detect( Descripc,"garage")
 parqueaderoT_aux8<-str_detect( Descripc,"garages") 
 parqueaderoT_aux9<-str_detect( Descripc,"garjes") 
 parqueaderoT_aux10<-str_detect( Descripc,"garje") 
-parqueaderoT<-ifelse(parqueaderoT_aux1==TRUE|parqueaderoT_aux2==TRUE| parqueaderoT_aux3==TRUE|parqueaderoT_aux4==TRUE|parqueaderoT_aux5==TRUE|parqueaderoT_aux6==TRUE|parqueaderoT_aux7==TRUE|parqueaderoT_aux8==TRUE|parqueaderoT_aux9 == TRUE |parqueaderoT_aux10==TRUE , 1,0 )
+parqueaderoT<-ifelse(parqueaderoT_aux1==TRUE|parqueaderoT_aux2==TRUE| parqueaderoT_aux3==TRUE|
+                       parqueaderoT_aux4==TRUE|parqueaderoT_aux5==TRUE|parqueaderoT_aux6==TRUE|
+                       parqueaderoT_aux7==TRUE|parqueaderoT_aux8==TRUE|parqueaderoT_aux9 == TRUE |
+                       parqueaderoT_aux10==TRUE , 1,0 )
 parqueaderoT<-data.frame(parqueaderoT)
 summary(parqueaderoT)
 parqueaderoT[is.na(parqueaderoT)] = 0 #Se imputa cero a los datos NA, porque existen datos donde no había descripción
 summary(parqueaderoT)
 combine_chapinero <- cbind(combine_chapinero, parqueaderoT)
 # Contar el número de veces que la variable toma el valor de "1"
-count_ascensor <- sum(combine_chapinero$ascensorT == 1)
-count_ascensor   ###2312 de 11222 hogares tienen ascensor (21%)
-  
-### Variable número 2: Parqueadero
+prop_parking <- round(sum(combine_chapinero$parqueaderoT == 1)/nrow(combine_chapinero)*100,1)
+paste("El",prop_parking, "%", "de apartamentos tienen parqueadero")
+
+# Limpiar ambiente
+rm(parqueaderoT_aux1,parqueaderoT_aux2,parqueaderoT_aux3,parqueaderoT_aux4,
+   parqueaderoT_aux5,parqueaderoT_aux6,parqueaderoT_aux7,parqueaderoT_aux8,
+   parqueaderoT_aux9,parqueaderoT_aux10)
+
+
+### Variable número 2: Elevador ####
 ascensorT_aux1<-str_detect( Descripc,"ascensor")
 ascensorT_aux2<-str_detect( Descripc,"acensor") 
 ascensorT_aux3<-str_detect( Descripc,"asensor") 
@@ -169,17 +185,23 @@ ascensorT_aux5<-str_detect( Descripc,"ascensores")
 ascensorT_aux6<-str_detect( Descripc,"acensores") 
 ascensorT_aux7<-str_detect( Descripc,"asensores") 
 ascensorT_aux8<-str_detect( Descripc,"elevadores") 
-ascensorT<-ifelse(ascensorT_aux1==TRUE|ascensorT_aux2==TRUE| ascensorT_aux3==TRUE|ascensorT_aux4==TRUE|ascensorT_aux5==TRUE|ascensorT_aux6==TRUE|ascensorT_aux7==TRUE|ascensorT_aux8==TRUE, 1,0 )
+ascensorT<-ifelse(ascensorT_aux1==TRUE|ascensorT_aux2==TRUE| ascensorT_aux3==TRUE|
+                    ascensorT_aux4==TRUE|ascensorT_aux5==TRUE|ascensorT_aux6==TRUE|
+                    ascensorT_aux7==TRUE|ascensorT_aux8==TRUE, 1,0 )
 ascensorT<-data.frame(ascensorT)
 summary(ascensorT)
 ascensorT[is.na(ascensorT)] = 0 #Se imputa cero a los datos NA, porque existen datos donde no había descripción
 summary(ascensorT)
 combine_chapinero <- cbind(combine_chapinero, ascensorT)
-count_parking <- sum(combine_chapinero$parqueaderoT == 1)
-count_parking   ###7733 de 11222 hogares tienen ascensor (69%)
+# Contar
+prop_ascensor <- round(sum(combine_chapinero$ascensorT == 1)/nrow(combine_chapinero)*100,1)
+paste("El",prop_ascensor, "%", "de apartamentos tienen ascensor")
 
+# Limpiar ambiente
+rm(ascensorT_aux1,ascensorT_aux2,ascensorT_aux3,ascensorT_aux4,
+   ascensorT_aux5,ascensorT_aux6,ascensorT_aux7,ascensorT_aux8)
 
-### Variable número 3: Baño privado
+### Variable número 3: Baño privado ####
 bañoprivado_aux1 <-str_detect( Descripc,"bano privado")
 bañoprivado_aux2 <-str_detect( Descripc,"baño privado")
 bañoprivado <-ifelse(bañoprivado_aux1==TRUE|bañoprivado_aux2==TRUE, 1,0 )
@@ -188,11 +210,15 @@ summary(bañoprivado)
 bañoprivado[is.na(bañoprivado)] = 0 #Se imputa cero a los datos NA, porque existen datos donde no había descripción
 summary(bañoprivado)
 combine_chapinero <- cbind(combine_chapinero, bañoprivado)
-count_baño <- sum(combine_chapinero$bañoprivado == 1)
-count_baño   ###1251 de 11222 hogares tienen ascensor (11%)
+# Contar
+prop_bano <- round(sum(combine_chapinero$bañoprivado == 1)/nrow(combine_chapinero)*100,1)
+paste("El",prop_bano, "%", "de apartamentos tienen baño privado")
+
+# Limpiar ambiente
+rm(bañoprivado_aux1,bañoprivado_aux2)
 
 
-### Variable número 4: Balcón/terraza
+### Variable número 4: Balcón/terraza ####
 balcon_aux1 <-str_detect( Descripc,"balcon")
 balcon_aux2 <-str_detect( Descripc,"balcón")
 balcon_aux3 <-str_detect( Descripc,"terraza")
@@ -202,20 +228,132 @@ balcon_aux6 <-str_detect( Descripc,"asotea")
 balcon_aux7 <-str_detect( Descripc,"cornisa")
 balcon_aux8 <-str_detect( Descripc,"corniza")
 balcon_aux9 <-str_detect( Descripc,"corniza")
-balcon <-ifelse(balcon_aux1==TRUE|balcon_aux2==TRUE| balcon_aux3==TRUE|balcon_aux4==TRUE|balcon_aux5==TRUE|balcon_aux6==TRUE|balcon_aux7==TRUE|balcon_aux8==TRUE|balcon_aux9==TRUE, 1,0 )
+balcon <-ifelse(balcon_aux1==TRUE|balcon_aux2==TRUE| balcon_aux3==TRUE|balcon_aux4==TRUE|
+                  balcon_aux5==TRUE|balcon_aux6==TRUE|balcon_aux7==TRUE|balcon_aux8==TRUE|
+                  balcon_aux9==TRUE, 1,0 )
 balcon <-data.frame(balcon)
 summary(balcon)
 balcon[is.na(balcon)] = 0 #Se imputa cero a los datos NA, porque existen datos donde no había descripción
 summary(balcon)
 combine_chapinero <- cbind(combine_chapinero, balcon)
-count_balcon <- sum(combine_chapinero$balcon == 1)
-count_balcon   ###4997 de 11222 hogares tienen ascensor (45%)
+# Contar
+prop_balcon <- round(sum(combine_chapinero$balcon == 1)/nrow(combine_chapinero)*100,1)
+paste("El",prop_balcon, "%", "de apartamentos tienen balcón")
+
+# Limpiar ambiente
+rm(balcon_aux1,balcon_aux2,balcon_aux3,balcon_aux4,balcon_aux5,balcon_aux6,
+   balcon_aux7,balcon_aux8,balcon_aux9)
+
+
+### Variable número 5: Vista ####
+vista_aux1 <-str_detect( Descripc,"vista")
+vista <-ifelse(vista_aux1==TRUE, 1,0 )
+vista <-data.frame(vista)
+summary(vista)
+vista[is.na(vista)] = 0 #Se imputa cero a los datos NA, porque existen datos donde no había descripción
+summary(vista)
+combine_chapinero <- cbind(combine_chapinero, vista)
+# Contar
+prop_vista <- round(sum(combine_chapinero$vista == 1)/nrow(combine_chapinero)*100,1)
+paste("El",prop_vista, "%", "de apartamentos tienen vista")
+
+# Limpiar ambiente
+rm(vista_aux1)
+
+
+### Variable número 6: Remodelado ####
+remod_aux1 <-str_detect( Descripc,"remodelado")
+remod_aux2 <-str_detect( Descripc,"remodelados")
+remod_aux3 <-str_detect( Descripc,"renovado")
+remod_aux4 <-str_detect( Descripc,"renobado")
+remod_aux5 <-str_detect( Descripc,"mejorado")
+remod_aux6 <-str_detect( Descripc,"moderno")
+remod_aux7 <-str_detect( Descripc,"modernos")
+remod_aux8 <-str_detect( Descripc,"modernizado")
+remod_aux9 <-str_detect( Descripc,"modernizados")
+remod_aux10 <-str_detect( Descripc,"integral")
+remod_aux11 <-str_detect( Descripc,"integrales")
+remodelado <-ifelse(remod_aux1==TRUE|remod_aux2==TRUE|remod_aux3==TRUE|remod_aux4==TRUE|
+                       remod_aux5==TRUE|remod_aux6==TRUE|remod_aux7==TRUE|remod_aux8==TRUE|
+                       remod_aux9==TRUE|remod_aux10==TRUE|remod_aux11==TRUE, 1,0 )
+remodelado <-data.frame(remodelado)
+summary(remodelado)
+remodelado[is.na(remodelado)] = 0 #Se imputa cero a los datos NA, porque existen datos donde no había descripción
+summary(remodelado)
+combine_chapinero <- cbind(combine_chapinero, remodelado)
+# Contar
+prop_remod <- round(sum(combine_chapinero$remodelado == 1)/nrow(combine_chapinero)*100,1)
+paste("El",prop_remod, "%", "de apartamentos está remodelado")
+
+# Limpiar ambiente
+rm(remod_aux1,remod_aux2,remod_aux3,remod_aux4,remod_aux5,remod_aux6,
+   remod_aux7,remod_aux8,remod_aux9,remod_aux10,remod_aux11)
+
+### Variable número 7: Admon incluida  ####
+admon_aux1 <-str_detect( Descripc,"administracion incluida")
+admon_aux2 <-str_detect( Descripc,"admon incluida")
+admon_aux3 <-str_detect( Descripc,"incluye administracion")
+admon_aux4 <-str_detect( Descripc,"incluye admon")
+admon_aux5 <-str_detect( Descripc,"administracion cubierta")
+admon_aux6 <-str_detect( Descripc,"administracion cubiertos")
+admon <-ifelse(admon_aux1==TRUE|admon_aux2==TRUE|admon_aux3==TRUE|admon_aux4==TRUE|
+                 admon_aux5==TRUE|admon_aux6==TRUE, 1,0 )
+admon <-data.frame(admon)
+summary(admon)
+admon[is.na(admon)] = 0 #Se imputa cero a los datos NA, porque existen datos donde no había descripción
+summary(admon)
+combine_chapinero <- cbind(combine_chapinero, admon)
+# Contar
+prop_admon <- round(sum(combine_chapinero$admon == 1)/nrow(combine_chapinero)*100,1)
+paste("El",prop_admon, "%", "de apartamentos incluyen administración")
+
+# Limpiar ambiente
+rm(admon_aux1,admon_aux2,admon_aux3,admon_aux4,admon_aux5,admon_aux6)
+
+
+### Tabla de proporciones final ####
+nombres <- c("Parqueadero", "Elevador","Baño privado", "Balcón","Vista",
+             "Remodelado","Admon incluida")
+valores <- c(prop_parking, prop_ascensor,prop_bano,prop_balcon,prop_vista,
+             prop_remod,prop_admon)
+
+Tabla_prop <- data.frame(Descripcion = nombres, Proporcion = valores)
+Tabla_prop
+
+
+
 
 
 # External data -----------------------------------------------------------
 
 # 
 
+
+
+
+
+# Como se juntaron las bases train y tes, luego se filtraron los datos  que solo son de chapinero
+# y se añadieron nuevas variables; para entrenar los modelos es necesario tener divididos el grupo
+# de entrenamiento y de control nuevamente. Por lo cual debemos realizar nuevamente esta división:
+
+### Train Group
+filtro <- is.na(combine_chapinero$price)
+Train_combine <- combine_chapinero[!filtro, ]
+
+# Grafica
+ggplot(Train_combine, aes(x = price)) +
+  geom_histogram(fill = "darkblue") +
+  theme_bw() +
+  labs(x = "Precio de arriendo", y = "Cantidad")
+
+#Descriptivas - Variable de interes (precio)
+summary(Train_combine$price) %>%
+  as.matrix() %>%
+  as.data.frame() %>%
+  mutate(V1 = scales::dollar(V1))
+
+### Test Group
+Test_combine <- combine_chapinero[filtro, ]
 
 
 
