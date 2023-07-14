fb751dd52115845b47896fe789ffade83c5c0ada
MiguelVictoria <m.victorias@uniandes.edu.co>
2023-07-14 09:17:42 -0500

    Update 2. Models.R

--- scripts/2. Models.R
+++ scripts/2. Models.R
@@ -38,42 +38,14 @@ p_load(tidyverse,rio,
 
 
 # Import data -------------------------------------------------------------
-load("../stores/data2.Rdata")
+load("../stores/data.Rdata")
 
 url_submission_template <- "https://raw.githubusercontent.com/irivelez/PS2_Making_Money_with_ML/master/stores/submission_template.csv"
 submission_template <- read.csv(url_submission_template)
 
-
-# Option 1: Groups ------------------------------------------------------------------
-
-# Como se juntaron las bases train y tes, luego se filtraron los datos  que solo son de chapinero
-# y se añadieron nuevas variables; para entrenar los modelos es necesario tener divididos el grupo
-# de entrenamiento y de control nuevamente. Por lo cual debemos realizar nuevamente esta división:
-
-### Train Group
-filtro <- is.na(combine_chapinero$price)
-Train_combine <- combine_chapinero[!filtro, ]
-
-# Grafica
-ggplot(Train_combine, aes(x = price)) +
-  geom_histogram(fill = "darkblue") +
-  theme_bw() +
-  labs(x = "Precio de venta", y = "Cantidad")
-
-#Descriptivas - Variable de interes (precio)
-summary(Train_combine$price) %>%
-  as.matrix() %>%
-  as.data.frame() %>%
-  mutate(V1 = scales::dollar(V1))
-
-### Test Group
-Test_combine <- combine_chapinero[filtro, ]
-
-
-
-
-# Option 2: Submission template  ---------------------------------------------------
 base <- left_join(submission_template, db, by = "property_id")
+base <- base %>% select(-price.y)
+base <-  base  %>%   drop_na(surface_covered_new) 
 
 
 
@@ -81,6 +53,7 @@ base <- left_join(submission_template, db, by = "property_id")
 
 # Create ln_price
 base$ln_price <- log(base$price.x)
+base[is.na(base)] <- 0
 
 # Train/Test
 set.seed(123)
@@ -89,21 +62,67 @@ sum(sample)/nrow(base)
 train  <- base[sample, ] 
 test   <- base[!sample, ] 
 
-
-
 # Models ------------------------------------------------------------------
+## Groups
+# Variables X
+names(base)
+X0 <- as.matrix(base  %>% select(-property_id,-geometry,
+                                 -price.x,-ln_price))
+X0[is.na(X0)] <- 0
+
+# Variable Y
+Y <- as.matrix(base$ln_price)
 
 
 ## LM ####
-## CONTINUAR CON MODELOS SIMPLES
-model1_lm<-lm(ln_price~1,data=train)
+### Modelo 1 lm ####
+model1_lm<-lm(ln_price~1, data = train)
 summary(model1_lm)
+coef(model1_lm)
 
-stargazer(model1_lm,
-          type = "text", 
-          dep.var.labels = "Precio del casa",
-          digits = 0
-)
+#prediction on new data
+test$yhat_model1_lm<-predict(model1_lm,newdata = test)
+MSE_model1_lm <- with(test,mean((ln_price-yhat_model1_lm)^2))
+MSE_model1_lm
+
+### Modelo 2 lm ####
+model2_lm<-lm(ln_price~surface_covered_new, data = train)
+summary(model2_lm)
+coef(model2_lm)
+
+#prediction on new data
+test$yhat_model2_lm<-predict(model2_lm,newdata = test)
+MSE_model2_lm <- with(test,mean((ln_price-yhat_model2_lm)^2))
+MSE_model2_lm
+
+### Modelo 3 lm ####
+model3_lm<-lm(ln_price~surface_covered_new+rooms+bedrooms+bathrooms+
+                parqueaderoT+ascensorT+bañoprivado+balcon+vista+remodelado+
+                Es_apartamento+distancia_parque+area_parque+distancia_sport_centre+
+                distancia_swimming_pool, data = train)
+summary(model3_lm)
+coef(model3_lm)
+
+#prediction on new data
+test$yhat_model3_lm<-predict(model3_lm,newdata = test)
+MSE_model3_lm <- with(test,mean((ln_price-yhat_model3_lm)^2))
+MSE_model3_lm
+
+
+
+# Create vars with all the MSE
+mse1_lm<-with(test,round(mean((ln_price-yhat_model1_lm)^2),20))
+mse2_lm<-with(test,round(mean((ln_price-yhat_model2_lm)^2),20))
+mse3_lm<-with(test,round(mean((ln_price-yhat_model3_lm)^2),20))
+
+
+#put them in a vector
+mse_lm<-c(mse1_lm,mse2_lm,mse3_lm)
+
+#create a data frame
+mse_lm_db<-data.frame(model=factor(c("model1","model2","model3"),ordered=TRUE),
+               MSE=mse_lm)
+mse_lm_db
 
 
 ## Ridge ####
@@ -111,14 +130,6 @@ stargazer(model1_lm,
 
 ## Lasso ####
 
-# Variables X
-names(base)
-X0 <- as.matrix(base  %>% select(-property_id,-price.y,-geometry,
-                               -price.x,-ln_price))
-X0[is.na(X0)] <- 0
-
-# Variable Y
-Y <- base$ln_price
   
 # Reg 1 Lasso
 lasso_no_pen <- glmnet(
